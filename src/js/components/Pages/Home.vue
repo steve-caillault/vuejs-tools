492095ab9894064ec9c6080ce1f4db00c88468b5
Stève Caillault <steve.caillault@gmail.com>
2023-02-23 20:54:55 +0100

    Composant de la page d'index.

---
+++ src/js/components/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+/***/
+import { useApplication } from '@compositions/Application';
+
+// Composants
+const Layout = defineAsyncComponent(() => import(/* webpackChunkName: "components-layouts" */ '@components/Layouts/Base.vue'));
+
+// Composition pour l'application
+const { applicationName } = useApplication();
+</script>
+
+<template>
+    <layout :page-title="applicationName"></layout>
+</template>
